d4095939d26191c32ee4d6eee4860c2d8d8668dd
arundaya <runsworkspace@gmail.com>
2025-04-05 20:53:18 +0700

    feat: added continous args iterator method

--- utils/args_iterator/args_iterator.go
+++ utils/args_iterator/args_iterator.go
@@ -13,12 +13,20 @@ type ArgsIterator struct {
   valid bool
 }
 
+func isFlag(input string) bool {
+	if (input[0] == '-') {
+		return true
+	}
+	return false
+}
+
 func NewArgsIterator (args []string) *ArgsIterator {
   return &ArgsIterator{
     args: args,
     valid: true,
   }
 }
+
 
 func (i *ArgsIterator) HasNext() bool {
   if (i.Index < len(i.args)) {
@@ -28,13 +36,26 @@ func (i *ArgsIterator) HasNext() bool {
 }
 
 func (i *ArgsIterator) GetNext() string {
-
   if i.HasNext() {
     arg:=i.args[i.Index]
     i.Index++
     return arg
   }
   return ""
+}
+
+func (i *ArgsIterator) GetNextContinous() string {
+	ret := ""
+	first := false
+	for i.HasNext() {
+		if isFlag(i.args[i.Index]) {
+		 return ret
+		}
+		if (!first) {ret += " "}
+		ret += i.args[i.Index]
+		i.Index++
+	}
+	return ret
 }
 
 func (i *ArgsIterator) GetNextInt(error *error) int {
@@ -52,3 +73,11 @@ func  (i *ArgsIterator) GetNextTime(error *error) *time.Time  {
 	return t
   }
 
+func (i* ArgsIterator) GetNextTimeContinous(error *error) *time.Time {
+	t, _err := time_utils.ParseUserTime(i.GetNextContinous())
+	if _err != nil {
+	error = &_err
+	return nil
+	}
+	return t
+}
